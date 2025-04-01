c4c98c27a44d0a68848f647624aad5062e308c58
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2025-04-01 07:34:28 -0300

    blog

--- src/controllers/category/CategoriesBlogController.ts
+++ src/controllers/category/CategoriesBlogController.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Request, Response } from "express";
 import { CategoriesBlogService } from "../../services/category/CategoriesBlogService";
 
@@ -14,21 +13,4 @@ class CategoriesBlogController {
   }
 }
 
-=======
-import { Request, Response } from "express";
-import { CategoriesBlogService } from "../../services/category/CategoriesBlogService";
-
-class CategoriesBlogController {
-  async handle(req: Request, res: Response) {
-    try {
-      const categorias_blog = await new CategoriesBlogService().execute();
-      return res.json(categorias_blog);
-    } catch (error) {
-      console.error("Erro no endpoint:", error);
-      return res.status(500).json({ error: "Falha ao carregar categorias" });
-    }
-  }
-}
-
->>>>>>> a1dcb2e4d383c73addf2ba89ceb9697537099208
 export { CategoriesBlogController };
--- src/services/category/CategoriesBlogService.ts
+++ src/services/category/CategoriesBlogService.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import prismaClient from "../../prisma";
 import { StatusCategory } from "@prisma/client";
 
@@ -23,30 +22,4 @@ class CategoriesBlogService {
   }
 }
 
-=======
-import prismaClient from "../../prisma";
-import { StatusCategory } from "@prisma/client";
-
-class CategoriesBlogService {
-  async execute() {
-    const all_categories = await prismaClient.category.findMany({
-      where: { status: StatusCategory.Disponivel },
-      orderBy: { created_at: "desc" },
-      include: {
-        children: {
-          where: { status: StatusCategory.Disponivel },
-          select: {
-            id: true,
-            slug_name_category: true,
-            name_category: true
-          }
-        }
-      }
-    });
-
-    return all_categories;
-  }
-}
-
->>>>>>> a1dcb2e4d383c73addf2ba89ceb9697537099208
 export { CategoriesBlogService };
